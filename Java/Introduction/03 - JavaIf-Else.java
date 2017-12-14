d83b1a20dba91f9ac1e58c71265ef67be3bb4e52
Ziya Erogul <ziya.erogul@HFs-MacBook-Pro.local>
2017-12-14 11:39:29 +0100

    Repository update.

---
+++ Java/Introduction/03 - JavaIf-Else.java
@@ -0,0 +1,50 @@
+//     Author: Z. Ilkem Erogul
+//     Github: github.com/ilkemerogul
+// HackerRank: hackerrank.com/ilkemerogul
+
+// Problem Statement
+/*
+Given an integer, n, perform the following conditional actions:
+
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+
+Complete the stub code provided in your editor to print whether or not n is weird.
+
+*/
+
+// Solution
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String ans="";
+        if(n%2==1){
+          ans = "Weird";
+        }
+        else{
+           //Complete the code
+           if (n%2==0 && n>=2 && n<=5){
+               ans = "Not Weird";
+           }
+           else if (n%2==0 && n>=6 && n<=20){
+               ans = "Weird";
+           }
+           else{
+               ans = "Not Weird";
+           }
+        }
+        System.out.println(ans);
+    }
+}
